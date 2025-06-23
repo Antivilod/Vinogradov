aa41f95a3ca15a41d122ea943c0b8a55b205f5da
Antivilod <vinogradovvdv@gmail.com>
2025-06-23 17:01:18 +0300

    Update Task3.java

--- src/Task3.java
+++ src/Task3.java
@@ -1,6 +1,6 @@
 public class Task3 {
     public static void run() {
-        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.println("Чётные числа в массиве:");
         for (int num : array) {
             if (num % 2 == 0) {
