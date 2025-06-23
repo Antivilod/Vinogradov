85abf6f66f9a9a74006b58ae43376e8e64f5606c
Antivilod <vinogradovvdv@gmail.com>
2025-06-23 16:28:40 +0300

    Create Task2.java

---
+++ src/Task2.java
@@ -0,0 +1,14 @@
+public class Task2 {
+    public static void run(Scanner scanner) {
+        System.out.print("Введите первую строку: ");
+        String a = scanner.nextLine();
+        System.out.print("Введите вторую строку: ");
+        String b = scanner.nextLine();
+
+        if (a.equals(b)) {
+            System.out.println("Строки идентичны");
+        } else {
+            System.out.println("Строки неидентичны");
+        }
+    }
+}
